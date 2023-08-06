2cd3b94848fb4deeabe019614b6e1d04c6271acc
Kaide <kaidewu@protonmail.com>
2023-08-06 22:56:57 +0200

    Add save file in the POST API REST Drive

--- cloudstation/api/api_v1/routers/drive/drive.py
+++ cloudstation/api/api_v1/routers/drive/drive.py
@@ -7,6 +7,7 @@ import traceback
 from sqlalchemy.orm import Session
 from db import crud, models
 from db.database import SessionLocal, engine
+from api.api_v1.routers.drive.save_files import save_files
 
 from api.api_v1.routers.drive.get_drive import get_drive
 from errors.error import Error
@@ -25,21 +26,23 @@ def get_db():
         db.close()
 
 
+def get_platform():
+    if platform.system() == "Windows":
+        basepath_csva_name = "BASEPATH-WINDOWS"
+    elif platform.system() in ("Linux", "Darwin"):
+        basepath_csva_name = "BASEPATH-LINUX"
+    else:
+        raise SystemError("The Operating System doesn't support!!")
+    return basepath_csva_name
+
 @drive_router.get("/drive/{drive_path:path}")
 async def drive(
         drive_path: str = "",
         orderby: str = Query(default=None, max_length=4),
         db: Session = Depends(get_db)
 ):
-    try:
-        if platform.system() == "Windows":
-            basepath_csva_name = "BASEPATH-WINDOWS"
-        elif platform.system() in ("Linux", "Darwin"):
-            basepath_csva_name = "BASEPATH-LINUX"
-        else:
-            raise SystemError("The Operating System doesn't support!!")
-        
-        basepath = crud.get_csva_default_values(db, csva_name=basepath_csva_name)
+    try:    
+        basepath = crud.get_csva_default_values(db, csva_name=get_platform())
         return get_drive(drive_path=drive_path, order_by=orderby, basepath=basepath[0])
     except:
         return Error(
@@ -53,9 +56,20 @@ async def drive(
 async def upload_drive(
     drive_path: str = "",
     files: Annotated[
-        list[UploadFile], File(description="Multiple files as UploadFile")
+        list[UploadFile], File(description="Upload Multiple files")
     ] = [],
+    db: Session = Depends(get_db)
 ):
-    if files != []:
-        return {"filenames": [file.filename for file in files], "Relative Path": drive_path}
-    return {"message": "File empty"}
+    try:
+        basepath = crud.get_csva_default_values(db, csva_name=get_platform())
+        if files != []:
+            return save_files(files, basepath[0], drive_path)
+        return {"message": "File empty"}
+    except:
+        return Error(
+            error_info=sys.exc_info(),
+            filename=__file__,
+            error_body="cloudstation.api.api_v1.routers.drive.drive -> upload_drive()",
+            error_traceback=traceback.format_exc()
+        ).error()
+    
---
+++ cloudstation/api/api_v1/routers/drive/save_files.py
@@ -0,0 +1,32 @@
+from errors.error import Error
+import os
+import shutil
+import sys
+import traceback
+
+def save_files(
+        files: list,
+        basepath: str,
+        path: str
+):
+    try:
+        files_exists = []
+        for file in files:
+            file_path = os.path.join(os.path.join(basepath, path), file.filename)
+            if os.path.exists(file_path):
+                files_exists.append(file)
+            if not os.path.isdir(os.path.join(basepath, path)):
+                os.makedirs(os.path.join(basepath, path))
+            with open(file_path, 'wb+') as f:
+                shutil.copyfileobj(file.file, f)
+    except:
+        return Error(
+            error_info=sys.exc_info(),
+            filename=__file__,
+            error_body="cloudstation.api.api_v1.routers.drive.save_files -> save_files()",
+            error_traceback=traceback.format_exc()
+        ).error()
+    finally:
+        return {
+            "message": f"Files {', '.join(file.filename for file in files_exists)} name already exists in the {path}" if len(files_exists) > 0 else "Successful"
+        }
